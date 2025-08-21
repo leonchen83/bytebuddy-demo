58b414261e665a64ca0baa46591a65a4b2c201b3
chenby <baoyi@nextop.asia>
2025-08-21 17:02:20 +0800

    Init

---
+++ src/main/java/org/example/dynamicproxy/ProxyFactory.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2016-2017 Leon Chen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.example.dynamicproxy;
+
+import static net.bytebuddy.dynamic.loading.ClassLoadingStrategy.Default.WRAPPER;
+import static net.bytebuddy.implementation.InvocationHandlerAdapter.of;
+import static net.bytebuddy.matcher.ElementMatchers.isDeclaredBy;
+import static net.bytebuddy.matcher.ElementMatchers.isDefaultMethod;
+import static net.bytebuddy.matcher.ElementMatchers.isFinalizer;
+import static net.bytebuddy.matcher.ElementMatchers.not;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.matcher.ElementMatchers;
+
+/**
+ * @author Baoyi Chen
+ */
+@SuppressWarnings("unchecked")
+public class ProxyFactory {
+	
+	public <T> T getProxy(Class<T> clazz, InvocationHandler handler) {
+		try {
+			if (clazz.isInterface()) {
+				return proxyInterface(clazz, handler).getConstructor().newInstance();
+			} else {
+				return proxyConcreate(clazz, handler).getConstructor().newInstance();
+			}
+		} catch (Throwable t) {
+			throw new InternalError(t);
+		}
+	}
+	
+	<T> Class<? extends T> proxyInterface(Class<T> c, InvocationHandler h) {
+		return (Class<? extends T>) new ByteBuddy().subclass((Object.class)).implement(c)
+				.method(isDeclaredBy(c).and(ElementMatchers.not(isDefaultMethod())))
+				.intercept(of(h)).make().load(c.getClassLoader(), WRAPPER).getLoaded();
+	}
+	
+	<T> Class<? extends T> proxyConcreate(Class<T> c, InvocationHandler h) {
+		return new ByteBuddy().subclass(c).method(not(isFinalizer())
+						.and(not(isDeclaredBy((Object.class)))).and(not(isDefaultMethod())))
+				.intercept(of(h)).make().load(c.getClassLoader(), WRAPPER).getLoaded();
+	}
+	
+	public static interface ITest {
+		void run();
+	}
+	
+	public static class Test implements ITest {
+		@Override
+		public void run() {
+			System.out.println("Test run method");
+		}
+	}
+	
+	public static class TestProxy extends Test {
+		private InvocationHandler handler;
+		
+		@Override
+		public void run() {
+			try {
+				handler.invoke(this, Test.class.getMethod("run"), null);
+			} catch (Throwable e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+	
+	static class XInvocationHandler implements InvocationHandler {
+		Object target;
+		
+		public XInvocationHandler(Object target) {
+			this.target = target;
+		}
+		
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			System.out.println("Running: " + method.getName());
+			return method.invoke(target, args);
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		Test target = new Test();
+		ProxyFactory factory = new ProxyFactory();
+		ITest test = factory.getProxy(ITest.class, new XInvocationHandler(target));
+		test.run();
+		
+		System.out.println("#".repeat(32));
+		
+		TestProxy proxy = new TestProxy();
+		proxy.handler = new XInvocationHandler(target);
+		proxy.run();
+	}
+}
